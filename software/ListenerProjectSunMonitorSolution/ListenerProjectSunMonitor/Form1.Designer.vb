4e8238dae5cbe4813422e80efe7a38d4b70eef74
sundberg86 <sompan86@gmail.com>
2016-09-13 21:18:57 +0200

    Laddat upp projekt.
    Laddat upp projekt för mjukvara.

---
+++ software/ListenerProjectSunMonitorSolution/ListenerProjectSunMonitor/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.labIP = New System.Windows.Forms.Label()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrListen = New System.Windows.Forms.Timer(Me.components)
+        Me.lblListen = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'labIP
+        '
+        Me.labIP.AutoSize = True
+        Me.labIP.Location = New System.Drawing.Point(238, 278)
+        Me.labIP.Name = "labIP"
+        Me.labIP.Size = New System.Drawing.Size(55, 13)
+        Me.labIP.TabIndex = 1
+        Me.labIP.Text = "IP Adress:"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(12, 278)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(40, 13)
+        Me.lblStatus.TabIndex = 2
+        Me.lblStatus.Text = "Status:"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 5
+        '
+        'tmrListen
+        '
+        Me.tmrListen.Enabled = True
+        '
+        'lblListen
+        '
+        Me.lblListen.AutoSize = True
+        Me.lblListen.Location = New System.Drawing.Point(13, 24)
+        Me.lblListen.Name = "lblListen"
+        Me.lblListen.Size = New System.Drawing.Size(43, 13)
+        Me.lblListen.TabIndex = 3
+        Me.lblListen.Text = "Indata: "
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ClientSize = New System.Drawing.Size(377, 300)
+        Me.Controls.Add(Me.lblListen)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.labIP)
+        Me.Name = "frmMain"
+        Me.Text = "All hearing ear"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents labIP As Label
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents tmrListen As Timer
+    Friend WithEvents lblListen As Label
+End Class
---
+++ software/ListenerProjectSunMonitorSolution/ListenerProjectSunMonitor/Form1.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Net
+Imports System.Text.Encoding
+
+Public Class frmMain
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            Dim IPhost As IPHostEntry = Dns.GetHostByName(Dns.GetHostName())
+            labIP.Text = "IP Adress: " & IPhost.AddressList(0).ToString()
+        Catch ex As Exception
+            MsgBox("There is an Error!")
+        End Try
+
+        Timer1.Enabled = True
+
+    End Sub
+
+
+    Private Function getcomputername() As String
+        Return Dns.GetHostName
+    End Function
+
+    Function ConnectionStatus() As Boolean
+        Return My.Computer.Network.Ping("8.8.8.8")
+    End Function
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            If ConnectionStatus() = True Then
+                lblStatus.Text = "Status: " & "Ansluten!"
+            ElseIf ConnectionStatus() = False Then
+                lblStatus.Text = "Status: " & "Ej ansluten!"
+            End If
+        Catch ex As Exception
+            lblStatus.Text = "Status: " & "Ej ansluten!"
+        End Try
+
+    End Sub
+
+    Private Sub tmrListen_Tick(sender As Object, e As EventArgs) Handles tmrListen.Tick
+
+    End Sub
+End Class
