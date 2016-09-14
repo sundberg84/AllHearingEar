545fe9483fb208bc89fb9a15a9eb1a89e8259e3c
sundberg86 <sompan86@gmail.com>
2016-09-14 22:36:14 +0200

    Uppdaterad källkod.
    Uppdaterad källkod av mjukvara som kan ta emot ljud och text via
    UPDprotkoll.

--- software/ListenerProjectSunMonitorSolution/ListenerProjectSunMonitor/Form1.Designer.vb
+++ software/ListenerProjectSunMonitorSolution/ListenerProjectSunMonitor/Form1.Designer.vb
@@ -28,12 +28,21 @@ Partial Class frmMain
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.tmrListen = New System.Windows.Forms.Timer(Me.components)
         Me.lblListen = New System.Windows.Forms.Label()
+        Me.Receive_Vox = New System.Windows.Forms.Button()
+        Me.Stop_Receiving = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CBAutostart = New System.Windows.Forms.CheckBox()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'labIP
         '
         Me.labIP.AutoSize = True
-        Me.labIP.Location = New System.Drawing.Point(238, 278)
+        Me.labIP.Location = New System.Drawing.Point(238, 235)
         Me.labIP.Name = "labIP"
         Me.labIP.Size = New System.Drawing.Size(55, 13)
         Me.labIP.TabIndex = 1
@@ -42,7 +51,7 @@ Partial Class frmMain
         'lblStatus
         '
         Me.lblStatus.AutoSize = True
-        Me.lblStatus.Location = New System.Drawing.Point(12, 278)
+        Me.lblStatus.Location = New System.Drawing.Point(12, 235)
         Me.lblStatus.Name = "lblStatus"
         Me.lblStatus.Size = New System.Drawing.Size(40, 13)
         Me.lblStatus.TabIndex = 2
@@ -65,17 +74,102 @@ Partial Class frmMain
         Me.lblListen.TabIndex = 3
         Me.lblListen.Text = "Indata: "
         '
+        'Receive_Vox
+        '
+        Me.Receive_Vox.Location = New System.Drawing.Point(16, 166)
+        Me.Receive_Vox.Name = "Receive_Vox"
+        Me.Receive_Vox.Size = New System.Drawing.Size(75, 23)
+        Me.Receive_Vox.TabIndex = 8
+        Me.Receive_Vox.Text = "Lyssna"
+        Me.Receive_Vox.UseVisualStyleBackColor = True
+        '
+        'Stop_Receiving
+        '
+        Me.Stop_Receiving.Enabled = False
+        Me.Stop_Receiving.Location = New System.Drawing.Point(97, 166)
+        Me.Stop_Receiving.Name = "Stop_Receiving"
+        Me.Stop_Receiving.Size = New System.Drawing.Size(75, 23)
+        Me.Stop_Receiving.TabIndex = 9
+        Me.Stop_Receiving.Text = "Sluta Lyssna"
+        Me.Stop_Receiving.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(16, 51)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(343, 109)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Data Recieved"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 87)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Label4"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 63)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Label3"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Label2"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Label1"
+        '
+        'CBAutostart
+        '
+        Me.CBAutostart.AutoSize = True
+        Me.CBAutostart.Location = New System.Drawing.Point(16, 195)
+        Me.CBAutostart.Name = "CBAutostart"
+        Me.CBAutostart.Size = New System.Drawing.Size(173, 17)
+        Me.CBAutostart.TabIndex = 11
+        Me.CBAutostart.Text = "Starta AHE när windows startar"
+        Me.CBAutostart.UseVisualStyleBackColor = True
+        '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ControlLightLight
-        Me.ClientSize = New System.Drawing.Size(377, 300)
+        Me.ClientSize = New System.Drawing.Size(371, 257)
+        Me.Controls.Add(Me.CBAutostart)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Stop_Receiving)
+        Me.Controls.Add(Me.Receive_Vox)
         Me.Controls.Add(Me.lblListen)
         Me.Controls.Add(Me.lblStatus)
         Me.Controls.Add(Me.labIP)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Name = "frmMain"
         Me.Text = "All hearing ear"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -86,4 +180,12 @@ Partial Class frmMain
     Friend WithEvents Timer1 As Timer
     Friend WithEvents tmrListen As Timer
     Friend WithEvents lblListen As Label
+    Friend WithEvents Receive_Vox As Button
+    Friend WithEvents Stop_Receiving As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CBAutostart As CheckBox
 End Class
--- software/ListenerProjectSunMonitorSolution/ListenerProjectSunMonitor/Form1.vb
+++ software/ListenerProjectSunMonitorSolution/ListenerProjectSunMonitor/Form1.vb
@@ -1,11 +1,26 @@
 ﻿Imports System.Net
 Imports System.Text.Encoding
+Imports System.Net.Sockets
 
 Public Class frmMain
 
     Dim subscriber As New Sockets.UdpClient(11000)
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.Text = "All Hearing Ear"
+        Me.Location = New Point(CInt((Screen.PrimaryScreen.WorkingArea.Width / 2) - (Me.Width / 2)), CInt((Screen.PrimaryScreen.WorkingArea.Height / 2) - (Me.Height / 2)))
+        Dim LocalIPList As System.Net.IPHostEntry = Dns.GetHostEntry(Environment.MachineName)
+        For Each LocalIP As IPAddress In LocalIPList.AddressList
+            If LocalIP.AddressFamily = AddressFamily.InterNetwork Then
+                VoxViaUDPIPAddress = LocalIP
+                VoxViaUDPRemoteIpEndPoint = New IPEndPoint(VoxViaUDPIPAddress, VoxViaUDPLogicalPortNumber)
+            End If
+        Next
+        Label1.Text = "Waiting"
+        Label2.Text = "Waiting"
+        Label3.Text = "Waiting"
+        Label4.Text = "Waiting"
 
         subscriber.Client.ReceiveTimeout = 100
         subscriber.Client.Blocking = False
@@ -22,6 +37,23 @@ Public Class frmMain
 
     End Sub
 
+    ' Abort/Close threads on Form close. Make sure you close Form using X button and not shutting down
+    ' using Visual Studio Debugger or threads may not abort/close.
+
+    Private Sub Form1_FormClosing(sender As Object, e As EventArgs) Handles Me.FormClosing
+        Try
+            AbortVoxViaUDPThread = True
+        Catch ex As Exception
+        End Try
+        Try
+            VoxViaUDP.Abort()
+        Catch ex As Exception
+        End Try
+        Try
+            VoxViaUDPRxClient.Close()
+        Catch ex As Exception
+        End Try
+    End Sub
 
     Private Function getcomputername() As String
         Return Dns.GetHostName
@@ -55,6 +87,162 @@ Public Class frmMain
             lblListen.Text = ASCII.GetString(rcvbytes)
         Catch ex As Exception
         End Try
+
+    End Sub
+
+    ' UDP receiver code _________________________________________________________________________________________
+
+    Dim VoxViaUDP As System.Threading.Thread
+    Dim VoxViaUDPIPAddress As IPAddress
+    Dim VoxViaUDPLogicalPortNumber As Integer = 11000
+    Dim VoxViaUDPRemoteIpEndPoint As System.Net.IPEndPoint
+    Dim VoxViaUDPRxClient As System.Net.Sockets.UdpClient
+
+    Private Sub Receive_Vox_Click(sender As Object, e As EventArgs) Handles Receive_Vox.Click
+        Dim LocalIPList As System.Net.IPHostEntry = Dns.GetHostEntry(Environment.MachineName)
+        For Each LocalIP As IPAddress In LocalIPList.AddressList
+            If LocalIP.AddressFamily = AddressFamily.InterNetwork Then
+                VoxViaUDPIPAddress = LocalIP
+                VoxViaUDPRemoteIpEndPoint = New IPEndPoint(VoxViaUDPIPAddress, VoxViaUDPLogicalPortNumber)
+            End If
+        Next
+        Try
+            VoxViaUDP.Abort()
+        Catch ex As Exception
+        End Try
+        Try
+            VoxViaUDPRxClient.Close()
+        Catch ex As Exception
+        End Try
+        AbortVoxViaUDPThread = False
+        VoxViaUDP = New System.Threading.Thread(AddressOf VoxViaUDPReceivingThread)
+        VoxViaUDP.Start()
+
+        Receive_Vox.Enabled = False
+        Stop_Receiving.Enabled = True
+    End Sub
+
+    Dim BytesRcvd As Integer = 0
+    Dim AbortVoxViaUDPThread As Boolean = False
+    Dim RcvdWaveFileBytes As New List(Of Byte()) ' A List of Byte Arrays per index, not just single bytes per index.
+
+    Private Sub VoxViaUDPReceivingThread()
+        Try
+            Do
+                If AbortVoxViaUDPThread = False Then
+                    VoxViaUDPRxClient = New UdpClient(VoxViaUDPLogicalPortNumber)
+                    RcvdWaveFileBytes.Add(VoxViaUDPRxClient.Receive(VoxViaUDPRemoteIpEndPoint))
+                    VoxViaUDPRxClient.Close()
+                Else
+                    Exit Do
+                End If
+                Invoke(New VoxViaUDPDelDelegate(AddressOf VoxViaUDPDel))
+            Loop
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Delegate Sub VoxViaUDPDelDelegate()
+
+    Private Sub VoxViaUDPDel()
+        If AbortVoxViaUDPThread = False Then
+            Label1.Text = "The current received byte count prior to removal and playing is " & (RcvdWaveFileBytes.Count * 1280).ToString & "."
+            If RcvdWaveFileBytes.Count > 50 Then ' (Testing at 2 seconds data) Should be 32000 bytes or 1 second of recorded data. 25 * 1280 bytes per rcvd packet = 32000 bytes.
+                CreateWaveHeaderAndPlay()
+            End If
+        End If
+    End Sub
+
+    ' Wave header
+
+    Dim FileType() As Byte = System.Text.ASCIIEncoding.ASCII.GetBytes("RIFF")          ' The ASCII text string "RIFF", 4 bytes.
+    Dim FileSize() As Byte                                                             ' The file size not including the "RIFF" description (4 bytes) and header type "WAVE" (4 bytes). This is file size - 8. Or Wave Data size + 36 which is all byte counts except RIFF and WAVE bytes.
+    Dim HeaderType() As Byte = System.Text.ASCIIEncoding.ASCII.GetBytes("WAVE")        ' The ASCII text string "WAVE", 4 bytes.
+    Dim Format() As Byte = System.Text.ASCIIEncoding.ASCII.GetBytes("fmt ")            ' The ASCII text string "fmt "(The space is also included), 4 bytes including one of anything.
+    Dim FormatLength() As Byte                                                         ' The size of the WAVE type format. This is usually 16 but can be other sizes. I use 16 for this code. 4 bytes.
+    Dim WaveTypePCM() As Byte                                                          ' Type of WAVE format. This is a PCM header = 01. Other values indicates some forms of compression. 2 bytes.
+    Dim Channels() As Byte                                                             ' Channels 1 for mono, 2 for stereo. 2 bytes.
+    Dim SamplesHertz() As Byte                                                         ' Samples Hz is number of samples per second, or frequency in hertz and is 4 bytes.
+    Dim BytesPerSecond() As Byte                                                       ' BytesPerSecond is "Number of channels * Samples per second * Bits per Sample / 8". 4 bytes.
+    Dim Align() As Byte                                                                ' Align is "Number of channels * Bits per Sample / 8". 2 bytes.
+    Dim BitsPerSample() As Byte                                                        ' Bits per sample is usually 32, 24, 16 or 8. Using 16 for this code. 2 bytes.
+    Dim DataDescHeader() As Byte = System.Text.ASCIIEncoding.ASCII.GetBytes("data")    ' The data description header is the ASCII text string "data". 4 bytes.
+    Dim SizeOfData() As Byte                                                           ' Number of bytes of data within the data section. 4 bytes.
+    Dim WaveData As New List(Of Byte)                                                  ' The wave data.
+    Dim CompleteWaveFile() As Byte
+    Dim NumberofChannels As Integer = 2
+    Dim SamplingRate As Integer = 8000
+
+    Private Sub CreateWaveHeaderAndPlay()
+        WaveData.Clear()
+        For i = 0 To 49
+            WaveData.AddRange(RcvdWaveFileBytes(i)) ' Add indexes 0 to 24 byte arrays to WaveData from RcvdWaveFileBytes
+        Next
+        Label2.Text = "WaveData created at " & Now.ToLongTimeString & "."
+        RcvdWaveFileBytes.RemoveRange(0, 50) ' Remove indexes 0 to 24 (25 total indexes) from RcvdWaveFileBytes
+        FileSize = WaveFileHelper(WaveData.Count + 36, 4)
+        FormatLength = WaveFileHelper(16, 4)
+        WaveTypePCM = WaveFileHelper(1, 2)
+        Channels = WaveFileHelper(NumberofChannels, 2)
+        SamplesHertz = WaveFileHelper(SamplingRate, 4)
+        Dim BPS As Integer = CInt(2 * SamplingRate * 16 / 8)
+        BytesPerSecond = WaveFileHelper(BPS, 4)
+        Dim AlignInfo As Integer = CInt(2 * 16 / 8)
+        Align = WaveFileHelper(AlignInfo, 2)
+        BitsPerSample = WaveFileHelper(16, 2)
+        SizeOfData = WaveFileHelper(WaveData.Count, 4)
+        Using MS As New IO.MemoryStream
+            MS.Write(FileType, 0, FileType.Length)             ' 4 bytes
+            MS.Write(FileSize, 0, FileSize.Length)             ' 4 bytes
+            MS.Write(HeaderType, 0, HeaderType.Length)         ' 4 bytes
+            MS.Write(Format, 0, Format.Length)                 ' 4 bytes
+            MS.Write(FormatLength, 0, FormatLength.Length)     ' 4 bytes
+            MS.Write(WaveTypePCM, 0, WaveTypePCM.Length)       ' 2 bytes
+            MS.Write(Channels, 0, Channels.Length)             ' 2 bytes
+            MS.Write(SamplesHertz, 0, SamplesHertz.Length)     ' 4 bytes
+            MS.Write(BytesPerSecond, 0, BytesPerSecond.Length) ' 4 bytes
+            MS.Write(Align, 0, Align.Length)                   ' 2 bytes
+            MS.Write(BitsPerSample, 0, BitsPerSample.Length)   ' 2 bytes
+            MS.Write(DataDescHeader, 0, DataDescHeader.Length) ' 4 bytes
+            MS.Write(SizeOfData, 0, SizeOfData.Length)         ' 4 bytes
+            MS.Write(WaveData.ToArray, 0, WaveData.Count)      ' Number of bytes determined by sampled data
+            CompleteWaveFile = MS.ToArray
+        End Using
+        Label3.Text = "SoundPlayer started at " & Now.ToLongTimeString & "."
+        Using MS As New IO.MemoryStream(CompleteWaveFile)
+            Dim player As New Media.SoundPlayer(MS)
+            player.Play()
+        End Using
+        Label4.Text = "SoundPlayer last finished at " & Now.ToLongTimeString & "."
+    End Sub
+
+    Private Function WaveFileHelper(ByRef Value As Integer, ByRef Length As Integer) As Byte()
+        Dim b() As Byte = BitConverter.GetBytes(Value)
+        Dim Helper As New List(Of Byte)
+        Helper = b.ToList
+        Return Helper.GetRange(0, Length).ToArray
+    End Function
+
+    Private Sub Stop_Receiving_Click(sender As Object, e As EventArgs) Handles Stop_Receiving.Click
+        Try
+            AbortVoxViaUDPThread = True
+        Catch ex As Exception
+        End Try
+        Try
+            VoxViaUDP.Abort()
+        Catch ex As Exception
+        End Try
+        Try
+            VoxViaUDPRxClient.Close()
+        Catch ex As Exception
+        End Try
+        Label1.Text = "Waiting"
+        Label2.Text = "Waiting"
+        Label3.Text = "Waiting"
+        Label4.Text = "Waiting"
+
+        Receive_Vox.Enabled = True
+        Stop_Receiving.Enabled = False
 
     End Sub
 
